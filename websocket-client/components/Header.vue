a6c95f7e912db1239443ab476e0bd5ca29c721f7
kkent030315 <hrn832@protonmail.com>
2021-03-29 11:37:08 +0900

    implement theme toggle control in the header

--- websocket-client/components/Header.vue
+++ websocket-client/components/Header.vue
@@ -1,8 +1,62 @@
-<template></template>
+<template>
+  <header class="header">
+    <div class="innerContainer">
+      <div class="header-left">
+        <WebSocketLogo width="40" height="40" class="header-left__logo" />
+      </div>
+      <div class="header-right">
+        <ToggleTheme />
+      </div>
+    </div>
+  </header>
+</template>
 
 <script>
-export default {};
+import WebSocketLogo from "@/assets/svg/websocket.svg?inline";
+import ToggleTheme from "@/components/ToggleTheme.vue";
+
+export default {
+  components: {
+    WebSocketLogo,
+    ToggleTheme,
+  },
+};
 </script>
 
 <style lang="scss">
+@import "./assets/sass/variables.scss";
+
+.header {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  .innerContainer {
+    justify-content: center;
+    flex-direction: row;
+  }
+
+  &-left,
+  &-right {
+    display: flex;
+    flex-direction: row;
+  }
+
+  &-left {
+    justify-content: flex-start;
+
+    &__logo {
+      color: $color-logo;
+      fill: $color-logo;
+      margin: 10px;
+    }
+  }
+
+  &-right {
+    width: 100%;
+    justify-content: flex-end;
+  }
+}
 </style>
---
+++ websocket-client/components/ToggleTheme.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="themeControl">
+    <button class="themeControl__button" @click="swapLightAndDarkMode">
+      <DarkThemeIcon v-if="isLightMode()" class="themeControl__button__icon" />
+      <LightThemeIcon v-else class="themeControl__button__icon" />
+    </button>
+  </div>
+</template>
+
+<script>
+import LightThemeIcon from "@/assets/svg/light.svg?inline";
+import DarkThemeIcon from "@/assets/svg/dark.svg?inline";
+
+export default {
+  components: {
+    LightThemeIcon,
+    DarkThemeIcon,
+  },
+  methods: {
+    isLightMode() {
+      const currentMode = this.$colorMode.preference;
+      return currentMode === "light";
+    },
+    swapLightAndDarkMode() {
+      this.$colorMode.preference = this.isLightMode() ? "dark" : "light";
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "./assets/sass/variables.scss";
+
+.themeControl {
+  display: flex;
+  flex-direction: row;
+
+  &__button {
+    padding: 10px;
+
+    &__icon {
+      color: $color-icon;
+    }
+  }
+}
+</style>
